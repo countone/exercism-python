97c2d1a814da17b9fac5e1e8645fb29e338500dd
countone <vinayvivek90@yahoo.com>
2018-07-12 17:58:30 +0530

    First Commit

---
+++ perfect_numbers.py
@@ -0,0 +1,17 @@
+def classify(number):
+
+	if number<1:
+		raise ValueError('ValueError')
+	
+	aliquot_sum=0
+	
+	for i in range(1,number//2+1):
+		if number%i==0:
+			aliquot_sum+=i
+
+	if aliquot_sum<number:
+		return "deficient"
+	elif aliquot_sum==number:
+		return "perfect"
+	else:
+		return "abundant"
