ab016d4812e123b604770c8c642fbc2a44a40a0d
countone <vinayvivek90@yahoo.com>
2018-07-16 15:15:21 +0530

    First Commit
    Design: 7:38
    Debug: 1:20

--- sublist.py
+++ sublist.py
@@ -1,2 +1,29 @@
+SUBLIST=1
+EQUAL=0
+SUPERLIST=2
+UNEQUAL=3
+
 def check_lists(first_list, second_list):
-    pass
+    
+    if len(first_list)==len(second_list):
+    	if first_list==second_list:
+    		return EQUAL
+    	else:
+    		return UNEQUAL
+
+    elif len(first_list)<len(second_list):
+    	if not first_list:
+    		return SUBLIST
+    	else:
+    		for i in range(len(second_list)):
+    			if first_list[0]==second_list[i]:
+    					if first_list==second_list[i:i+len(first_list)]:
+    						return SUBLIST
+    		else:
+    			return UNEQUAL
+    elif len(first_list)>len(second_list):
+
+    	if check_lists(second_list,first_list)==SUBLIST:
+    		return SUPERLIST
+    	else:
+    		return UNEQUAL
